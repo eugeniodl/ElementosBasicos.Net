417e78fd9efe1b4e9a3f7bdbdc5e0dfca1587eb3
JADPA-21 <JADPA-21@JADPAS-21>
2022-08-04 10:56:57 -0600

    Agregar archivos de proyecto.

---
+++ CapitalEmpresa/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine(" ** CAPITAL EMPRESA ** ");
+Console.WriteLine(" ");
+
+double monto1, monto2, monto3;
+
+Console.WriteLine("Ingrese monto socio 01: C$");
+monto1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese monto socio 02: C$");
+monto2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese monto socio 03: C$");
+monto3 = double.Parse(Console.ReadLine());
+
+double capital = monto1 + monto2 + monto3;
+
+double pSocio1 = (monto1 * 100) / capital;
+double pSocio2 = (monto2 * 100) / capital;
+double pSocio3 = (monto3 * 100) / capital;
+Console.WriteLine("-----------------------------------");
+Console.WriteLine("CAPITAL: C$ " + capital.ToString("0.00"));
+Console.WriteLine("PORCENTAJE SOCIO 1: " + pSocio1.ToString("0.00"));
+Console.WriteLine("PORCENTAJE SOCIO 2: " + pSocio2.ToString("0.00"));
+Console.WriteLine("PORCENTAJE SOCIO 3: " + pSocio3.ToString("0.00"));
---
+++ ConversionMedidas/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine(" ** CONVERSIÓN DE MEDIDAS - EMP. TEXTIL ** ");
+Console.WriteLine(" ");
+
+double metros;
+
+Console.WriteLine("Ingrese la cantidad de metros: ");
+metros = double.Parse(Console.ReadLine());
+
+double centimetros = metros * 100.0;
+double pulgadas = centimetros / 2.54;
+double pies = pulgadas / 12.0;
+double yardas = pies / 3.0;
+
+Console.WriteLine("-------------------------------");
+Console.WriteLine("CENTÍMETROS: " + centimetros.ToString("0.00"));
+Console.WriteLine("PULGADAS: " + pulgadas.ToString("0.00"));
+Console.WriteLine("PIES: " + pies.ToString("0.00"));
+Console.WriteLine("YARDAS: " + yardas.ToString("0.00"));
